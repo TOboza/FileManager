931bbac8a0f5dd277b5b810a8e9f20ac062bc95a
Tomasz Oboza <t.oboza@hotmail.com>
2019-01-27 10:15:21 +0100

    Initializing commit - empty projcet

---
+++ src/main/java/com/tomaszoboza/filemanager/FilemanagerApplication.java
@@ -0,0 +1,14 @@
+package com.tomaszoboza.filemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilemanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilemanagerApplication.class, args);
+    }
+
+}
+
